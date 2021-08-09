25d77dfc05e9c9c5dad9e3c83d93740c7e6b0fa0
Crowdbotics <team@crowdbotics.com>
2021-08-09 08:12:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "offline_learning_29658Identifier",
+  url: "https://offline_learning_29658Identifier.botics.co",
+  api: "https://offline_learning_29658Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
